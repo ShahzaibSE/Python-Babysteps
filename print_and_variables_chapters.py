c07d553e37ba968c7f9d24fb64c9d7227541ed4f
ShahzaibSE <shahzaibanalyst@outlook.com>
2019-07-17 00:04:26 +0500

    Variables and Print

---
+++ print_and_variables_chapters.py
@@ -0,0 +1,23 @@
+print("Hello World");  # Hello World 
+'''
+ This is multiline comment.
+ Semi colon doesn't generate error.
+'''
+# Print("Hello World") # This generates error.
+#
+'''
+~~~  Working with variables ~~~
+'''
+#
+# We don't need declaration keyword for variables.
+name = "Shahzaib Noor" # At runtime it's type will be determined as string.
+print(name) #Simply printing name.
+print("My name is"+" "+name) # String concatenation.
+print(f"My name is {name}") #String Interpolation.
+#-------------------#
+# Ways to declare variables.
+# $myname="Bruce Wayne" #Wrong declaration.
+# print($myname)
+#
+# Note: Python is dynamically typed.
+# country-of-origin = "Pakistan" #Wrong declaration.
